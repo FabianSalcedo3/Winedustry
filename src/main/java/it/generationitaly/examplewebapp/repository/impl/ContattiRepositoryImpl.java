3c9834f211cc534727044e3a42950d52205fab7a
fabia <fabia@DESKTOP-H28007R.Home>
2023-01-11 14:58:48 +0100

    Aggiunto effetivamente metodo findaAll contatti

--- src/main/java/it/generationitaly/examplewebapp/repository/impl/ContattiRepositoryImpl.java
+++ src/main/java/it/generationitaly/examplewebapp/repository/impl/ContattiRepositoryImpl.java
@@ -2,10 +2,18 @@ package it.generationitaly.examplewebapp.repository.impl;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 import it.generationitaly.examplewebapp.entity.Contatti;
+import it.generationitaly.examplewebapp.entity.Studente;
 import it.generationitaly.examplewebapp.repository.ContattiRepository;
 
 public class ContattiRepositoryImpl implements ContattiRepository {
+
+	private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("persistence");
 
 	@Override
 	public Contatti findById(int id) {
@@ -15,8 +23,24 @@ public class ContattiRepositoryImpl implements ContattiRepository {
 
 	@Override
 	public List<Contatti> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Contatti> contatti = null;
+		EntityManager em = null;
+		EntityTransaction tx = null;
+		try {
+			em = emf.createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+			contatti = em.createQuery("from Contatti", Contatti.class).getResultList();
+			tx.commit();
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+			if (tx != null && tx.isActive())
+				tx.rollback();
+		} finally {
+			if (em != null)
+				em.close();
+		}
+		return contatti;
 	}
 
 }
