b5986c16886241342f781ae343e84a84804c6330
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 <bonfy.gaming@gmail.com>
2023-01-19 17:20:15 +0100

    Test per merge

--- src/main/webapp/home/script.js
+++
@@ -1,6 +0,0 @@
-$('.burger, .overlay').click(function(){
-  $('.burger').toggleClass('clicked');
-  $('.overlay').toggleClass('show');
-  $('nav').toggleClass('show');
-  $('body').toggleClass('overflow');
-});
