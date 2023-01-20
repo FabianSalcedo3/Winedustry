9d584451ceff22be63c14f443461608f97ce5587
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 <bonfy.gaming@gmail.com>
2023-01-20 12:13:47 +0100

    aa

--- src/main/java/controller/Init.java
+++ src/main/java/controller/Init.java
@@ -1,9 +1,16 @@
 package controller;
 
+import repository.utente.UtenteJPA;
+
 public class Init {
 
     public static void main(String[] args) {
-
+    	new UtenteJPA().findAll().forEach(utente ->{
+    		System.out.println(utente);
+    		System.out.println(utente.getIndirizzo());
+    		System.out.println(utente.getInformazioni());
+    		System.out.println(utente.getPagamento());
+    	});
     }
 
 }
